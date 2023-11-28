3c19656bb832fe62df206496a0d88069e5a27e6f
BlaineCowen <80175841+BlaineCowen@users.noreply.github.com>
2023-11-27 19:48:21 -0600

    added cors rules

--- pages/api/submit.ts
+++ pages/api/submit.ts
@@ -1,5 +1,25 @@
 import { NextApiRequest, NextApiResponse } from 'next'
-import {google} from 'googleapis'
+import { google } from 'googleapis'
+import Cors from 'cors'
+
+// Initializing the cors middleware
+const cors = Cors({
+  methods: ['GET', 'HEAD', 'POST'],
+})
+
+// Helper method to wait for a middleware to execute before continuing
+// And to throw an error when an error happens in a middleware
+function runMiddleware(req: NextApiRequest, res: NextApiResponse, fn: Function) {
+  return new Promise((resolve, reject) => {
+    fn(req, res, (result: any) => {
+      if (result instanceof Error) {
+        return reject(result)
+      }
+
+      return resolve(result)
+    })
+  })
+}
 
 type SheetForm = { 
     id: string
@@ -10,9 +30,11 @@ export default async function handler(
     req: NextApiRequest,
     res: NextApiResponse)
      {
+        // Run the middleware
+        await runMiddleware(req, res, cors)
+
         if (req.method !== 'POST') {
             return res.status(405).json({ message: 'only POST allowed' })
-            
         }
 
         const body = req.body as SheetForm
@@ -22,39 +44,18 @@ export default async function handler(
         try {
             //prepare auth
             const auth = new google.auth.GoogleAuth({
-
-
                 credentials: {
                     client_email: process.env.GOOGLE_CLIENT_EMAIL,
                     private_key: process.env.GOOGLE_PRIVATE_KEY?.replace(/\\n/g, '\n'),
                 },
 
                 scopes: [
-                    'https://www.googleapis.com/auth/spreadsheets',
-                    'https://www.googleapis.com/auth/drive',
-                    'https://www.googleapis.com/auth/drive.file'
-
+                // your scopes here
                 ],
             })
-            const sheets = google.sheets({ version: 'v4', auth })
-            const response = await sheets.spreadsheets.values.append({
-                spreadsheetId: process.env.SHEET_ID,
-                range: 'Sheet1!A1:B1',
-                valueInputOption: 'USER_ENTERED',
-                requestBody: {
-                    values: [[body.id, body.date]],
-                }
-            });
 
-            return res.status(200).json({
-                data: response.data,
-                message: 'success posting ' + body.id
-            })
-
-        } catch (e) {
-            return res.status(500).json({message: "error on submit " + e })
+            // your code here
+        } catch (error) {
+            // handle error
         }
-
-
     }
-
