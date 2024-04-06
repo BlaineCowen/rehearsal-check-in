abf45050cc5a326a1de896cb348356c5ff3cc34f
BlaineCowen <80175841+BlaineCowen@users.noreply.github.com>
2024-04-05 23:38:20 -0500

    add offline check

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -31,9 +31,27 @@ export default function Home({
 }) {
   const [currentPage, setCurrentPage] = useState<string>("Attendance");
 
+  const [isOnline, setIsOnline] = useState(
+    typeof window !== "undefined" ? navigator.onLine : true
+  );
+
   const handleButtonClick = (pageName: string) => {
     setCurrentPage(pageName);
   };
+
+  useEffect(() => {
+    function updateOnlineStatus() {
+      setIsOnline(navigator.onLine);
+    }
+
+    window.addEventListener("online", updateOnlineStatus);
+    window.addEventListener("offline", updateOnlineStatus);
+
+    return () => {
+      window.removeEventListener("online", updateOnlineStatus);
+      window.removeEventListener("offline", updateOnlineStatus);
+    };
+  }, []);
 
   return (
     <html lang="en">
@@ -43,8 +61,14 @@ export default function Home({
             onOptionClick={handleButtonClick}
             className="fixed h-16 bg-black z-10 w-full justify-center"
           />
-
-          <Attendance_Page currentPage={currentPage} />
+          <h1 className="text-4xl text-center mt-8"></h1>
+          {isOnline ? (
+            <Attendance_Page currentPage={currentPage} />
+          ) : (
+            <div className="flex justify-center items-center h-screen">
+              <h1 className="text-4xl text-center">You are offline</h1>
+            </div>
+          )}
         </body>
       </main>
     </html>
